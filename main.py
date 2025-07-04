fd67a3659e7370d2544550b6dc3d30c2627ed028
ygorabreu1512 <ygorabreu14@gmail.com>
2025-07-04 14:21:42 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="216.24.60.0/24", port=port)
